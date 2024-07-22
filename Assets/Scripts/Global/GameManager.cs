4e949824cc4ab4ef8854e7e77fb76e573a29ee90
MFBarker <98473836+MFBarker@users.noreply.github.com>
2024-07-22 17:25:19 -0600

    Restaurant Buttons Functionality and other small changes

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -28,11 +28,6 @@ public class GameManager : MonoBehaviour
         End
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/Restaurant/UIButtons.cs
+++ Assets/Scripts/Restaurant/UIButtons.cs
@@ -1,45 +1,135 @@
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.FilePathAttribute;
 
 public class UIButtons : MonoBehaviour
 {
-    public enum RButtons
+    enum RButtons
     { 
         Kitchen,
         Bar,
         Drinks
     }
 
-    [SerializeField] RButtons buttonType { get; set; }
-    Camera camera = Camera.main;
-
-    public Vector3 kitchen = new Vector3(-25, 0, 0);
-    Vector3 bar = new Vector3(0, 0, 0);
-    Vector3 drinks = new Vector3(25, 0, 0);
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    enum ButtonLocation
+    { 
+        Left,
+        Right
     }
 
+    [SerializeField] ButtonLocation buttonLocation;
+    [SerializeField] Button buttonObjectLeft;
+    [SerializeField] Button buttonObjectRight;
+    Camera camera = null;
+
+    Vector3 kitchen = new Vector3(-25, 0, -10);
+    Vector3 bar = new Vector3(0, 0, -10);
+    Vector3 drinks = new Vector3(25, 0, -10);
+
+    /*
+             Restaurant Layout 
+
+     |  Kitchen --- Bar --- Drinks  |
+     
+     */
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        /* Gets Secondary Camera to Move Around */
+        camera = GameObject.FindGameObjectWithTag("GameCamera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (camera.transform.position == kitchen && buttonObjectLeft.IsActive()) HideButton("Left");
+        if (camera.transform.position == drinks && buttonObjectRight.IsActive()) HideButton("Right");
+    }
+
+    /// <summary>
+    /// When button is clicked, the game will move left or right, depending on what button is pressed.
+    /// </summary>
     public void OnButtonClick()
+    {
+        switch (buttonLocation)
+        {
+            case ButtonLocation.Left:
+                //Kitchen Hides Buttons (Update)
+                if (camera.transform.position == bar)
+                {
+                    LocationChange(RButtons.Kitchen); 
+                }
+                else //drinks
+                {
+                    //Unhide Right, Go to bar
+                    UnHideButton("Right");
+                    LocationChange(RButtons.Bar);
+                }
+                break;
+            case ButtonLocation.Right:
+                if (camera.transform.position == kitchen) 
+                {
+                    //unhide left, go to bar
+                    UnHideButton("Left");
+                    LocationChange(RButtons.Bar);
+                }
+                else if (camera.transform.position == bar)
+                {
+                    LocationChange(RButtons.Drinks);
+                }
+                //Drinks Hides Button (Update)
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Hides Button based on Location
+    /// </summary>
+    /// <param name="location">the location of the button to be hidden</param>
+    private void HideButton(string location)
+    { 
+        if (location == "Left") buttonObjectLeft.gameObject.SetActive(false);
+        if (location == "Right") buttonObjectRight.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// Unhides Button based on Location
+    /// </summary>
+    /// <param name="location">the location of the button to be unhidden</param>
+    private void UnHideButton(string location)
+    {
+        if (location == "Left") buttonObjectLeft.gameObject.SetActive(true);
+        if (location == "Right") buttonObjectRight.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// Moves the camera based on where the player needs to go.
+    /// </summary>
+    /// <param name="buttonType">Where the player is moving to.</param>
+    private void LocationChange(RButtons buttonType)
     {
         switch (buttonType)
         {
             case RButtons.Kitchen:
                 //-25,0
-                camera.transform.transform.localPosition = kitchen;
+                Debug.Log("To Kitchen");
+                camera.transform.position = kitchen;
                 break;
             case RButtons.Bar:
                 //0,0
-                camera.transform.transform.localPosition = bar;
+                Debug.Log("To Bar");
+                camera.transform.position = bar;
                 break;
             case RButtons.Drinks:
                 //25,0
-                camera.transform.transform.localPosition = drinks;
+                Debug.Log("To Drinks");
+                camera.transform.position = drinks;
                 break;
             default:
                 break;
