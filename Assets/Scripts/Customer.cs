9a1071917abf45f591b1801e67f4d8c91d77130c
MFBarker <98473836+MFBarker@users.noreply.github.com>
2024-08-07 21:35:20 -0600

    Customer Info Implementation Progress (code) + Default Portrait added

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AdaptivePerformance.Provider;
+
+public enum CustomerPersonality
+{
+    Grumpy,
+    Polite,
+    Prideful,
+    Outgoing
+}
+
+[Serializable]
+public class Customer
+{
+    public string name;
+    public Texture2D picture;
+    public string occupation;
+    public CustomerPersonality personality;
+
+    /*
+    https://stackoverflow.com/questions/26043161/how-to-reference-a-texture-with-a-string
+    CS0619: Resources.LoadAssetAtPath is obsolete => AssetDatabase.LoadAssetAtPath
+     */
+    static string texture = "Assets/Art/Characters/Default.png";
+    Texture2D defaultTexture = AssetDatabase.LoadAssetAtPath<Texture2D>(texture);
+
+    public Customer(string name, string occupation, CustomerPersonality personality, Texture2D picture)
+    {
+        this.name = name;
+        this.picture = (picture != null) ? picture : defaultTexture; //if picture is null, use default texture 
+        this.occupation = occupation;
+        this.personality = personality;
+    }
+}
--- Assets/Scripts/Customers.cs
+++ Assets/Scripts/Customers.cs
@@ -1,21 +1,20 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-[Serializable]
-public class Customer
-{
-    public string name;
-    public Texture2D picture;
-    public string occupation;
-    public string personality;
-}
+
 
 public class Customers : MonoBehaviour
 {
-    public Customer[] customers;
+    [SerializeField]
+    public Customer[] customers = {
+        new Customer("... Tanaka","Farmer", CustomerPersonality.Grumpy, null),
+        new Customer("Haruto Nakamura","Salaryman", CustomerPersonality.Polite, null),
+        new Customer("... Soma","Manager", CustomerPersonality.Prideful, null),
+        new Customer("Logan Smith","English Teacher", CustomerPersonality.Outgoing, null)
+    };
+
 
     // Start is called before the first frame update
     void Start()
